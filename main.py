496c53f69de5a8b5225f63df7e2f8e2159b89069
c3rberuss <c3rberuss@gmail.com>
2018-04-13 20:27:05 -0600

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
+import sys
+
+app = QApplication(sys.argv)
+
+class main(QMainWindow):
+
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        uic.loadUi('interfaz/mainwindow.ui', self)
+
+
+main = main()
+main.show()
+
+app.exec_()
